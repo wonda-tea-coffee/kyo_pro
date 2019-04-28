b488e6443465c006ffa82026ed8a0983815317de
wonda-tea-coffee <rikita.ishikawa@crowdworks.co.jp>
2019-04-28 19:08:33 +0900

    q6、完全理解

--- math_puzzle_more/q6.rb
+++ math_puzzle_more/q6.rb
@@ -1,21 +1,15 @@
-# 長辺の長さが1000以下正方形が20個
-
-s = Time.now
-@memo = {}
-
 def g(n, m, k)
-  return @memo[[n, m, k]] if @memo[[n, m, k]]
-  return @memo[[n, m, k]] = @memo[[m, n, k]] if @memo[[m, n, k]]
   return 0 if n * m < k
   return k == 1 ? 1 : 0 if n == m
 
-  return @memo[[n, m, k]] = g([n - m, m].max, [n - m, m].min, k - 1)
+  return n - m >= m ? g(n - m, m, k - 1) : g(m, n - m, k - 1)
 end
 
 def f(n, m)
   ans = 0
   2.upto(n){|a|
-    1.upto(a-1){|b|
+    lim_a = a - 1
+    1.upto(lim_a){|b|
       ans += g(a, b, m)
     }
   }
@@ -23,16 +17,3 @@ def f(n, m)
 end
 
 puts f(1000, 20)
-puts "#{Time.now - s}s"
-
-# f(n, m)
-# a / gcd(a, b) = m
-# a = m*gcd(a, b)
-
-# f(8, 5)
-# a / gcd(a, b) = 5
-# a = 5*gcd(a, b)
-
-# a = 5のとき
-# 1 = gcd(5, b)
-# b = 1, 2, 3, 4
